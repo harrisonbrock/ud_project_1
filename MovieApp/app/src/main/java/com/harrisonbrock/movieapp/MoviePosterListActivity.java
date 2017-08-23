93d0c3ccc5e76ef847b2e81d3dd490bb9208d0f0
Harrison Brock <harrison@harrisonbrock.com>
2017-08-23 12:12:55 -0400

    Start of project

---
+++ MovieApp/app/src/main/java/com/harrisonbrock/movieapp/MoviePosterListActivity.java
@@ -0,0 +1,13 @@
+package com.harrisonbrock.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviePosterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_poster_list);
+    }
+}
