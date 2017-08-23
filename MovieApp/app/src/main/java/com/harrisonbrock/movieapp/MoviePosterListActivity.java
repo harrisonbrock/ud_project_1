9bf5ea1ed7234cb8591f2b70905109ca6387c972
Harrison Brock <harrison@harrisonbrock.com>
2017-08-23 18:00:18 -0400

    clean up

--- MovieApp/app/src/main/java/com/harrisonbrock/movieapp/MoviePosterListActivity.java
+++ MovieApp/app/src/main/java/com/harrisonbrock/movieapp/MoviePosterListActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Spinner;
 
 import com.harrisonbrock.movieapp.Model.Movie;
 
@@ -18,13 +20,25 @@ public class MoviePosterListActivity extends AppCompatActivity
 implements LoaderManager.LoaderCallbacks<List<Movie>>{
 
     private GridView mGridView;
+    private Spinner mSpinner;
     private MoviePosterAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_poster_list);
 
-        mGridView = (GridView) findViewById(R.id.grid);
+
+        createUI();
+
+        List<String> sortByList = new ArrayList<>();
+
+        sortByList.add("popular");
+        sortByList.add("top_rated");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, sortByList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(arrayAdapter);
 
         mAdapter = new MoviePosterAdapter(this, new ArrayList<Movie>());
 
@@ -43,6 +57,14 @@ implements LoaderManager.LoaderCallbacks<List<Movie>>{
                 startActivity(intent);
             }
         });
+    }
+
+    private void createUI() {
+
+        mGridView = (GridView) findViewById(R.id.grid);
+
+        mSpinner = (Spinner) findViewById(R.id.sp_sortBy);
+
     }
 
     @Override
