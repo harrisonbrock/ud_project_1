72a5585d18c8af327dedf93b2ac09868ab367481
Harrison Brock <harrison@harrisonbrock.com>
2017-08-24 14:23:12 -0400

    Add Item selected listener

--- MovieApp/app/src/main/java/com/harrisonbrock/movieapp/MoviePosterListActivity.java
+++ MovieApp/app/src/main/java/com/harrisonbrock/movieapp/MoviePosterListActivity.java
@@ -64,6 +64,19 @@ implements LoaderManager.LoaderCallbacks<List<Movie>>{
             }
         });
 
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
     private void createUI() {
